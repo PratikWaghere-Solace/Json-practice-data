24e5c73562623d5c3598bdf9863ba8fb3efea84e
PratikWaghere-Solace <pratik.solace123@gmail.com>
2024-10-16 18:38:52 +0530

    Added new feature to improve performance

--- vairable_array.php
+++ vairable_array.php
@@ -105,6 +105,9 @@ $shiping_data = json_encode($shiping, JSON_PRETTY_PRINT);
 $file2 = 'shipping_profile_feed.json';
 file_put_contents($file2, $shiping_data);
 
+
+sleep(20);
+
 exec('git status',$output1,$result1);
 echo '<pre>';
 print_r($output1);
